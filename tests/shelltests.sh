77823b9e74a00deb6b17192ef2f5a9f39a96dd36
Matt Haggard <haggardii@gmail.com>
2016-08-01 12:41:38 -0600

    Update shelltests

--- tests/shelltests.sh
+++ tests/shelltests.sh
@@ -4,5 +4,5 @@ hc=${1:-$(which humancrypto)}
 
 set -xe
 $hc --help
-echo 'mypassword' | $hc pw store2016 > /tmp/stored.txt
-echo 'mypassword' | $hc pw verify "$(cat /tmp/stored.txt)"
+echo 'mypassword' | $hc pw 2016 store > /tmp/stored.txt
+echo 'mypassword' | $hc pw 2016 verify "$(cat /tmp/stored.txt)"
